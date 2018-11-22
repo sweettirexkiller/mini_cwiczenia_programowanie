88863157fb042b345da110124d0b12da3d39fb14
sweettirexkiller <piotrjankiewicz02@gmail.com>
2018-11-22 09:18:40 +0100

    ...

--- 22_11_2018_exercise.c
+++ 22_11_2018_exercise.c
@@ -3,10 +3,19 @@
 //
 
 #include "stdio.h"
+#define N 10
 
 int main(void) {
 //    if(n & 0x1) // is odd
+    int a = 5, b = 3;
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
 
+    printf("%d %d %d\n", a, b, N);
+    puts("finished");
+//    printf("%s\n", "finished");
 
     return 0;
 }
